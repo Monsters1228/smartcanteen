108141bef6528ae431b59c3ea86cbd315193c8c2
黄世强 <2859456720@qq.com>
2022-04-14 17:29:16 +0800

    fix：face模块添加微服务框架

--- faceapi/src/main/java/com/zsc/FaceapiApplication.java
+++ faceapi/src/main/java/com/zsc/FaceapiApplication.java
@@ -1,9 +1,11 @@
-package com.zsc.faceapi;
+package com.zsc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FaceapiApplication {
 
     public static void main(String[] args) {

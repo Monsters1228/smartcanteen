e185d438b8f9b5b6ffdec3c60b50d6cbe94248b9
黄世强 <2859456720@qq.com>
2022-04-14 16:59:04 +0800

    fix:添加 face 模块

---
+++ faceapi/src/test/java/com/zsc/faceapi/FaceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsc.faceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
